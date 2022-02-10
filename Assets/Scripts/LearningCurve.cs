75cbf4a9b9ac8545d3ce1c222689412de12495fe
Joe Kane <jokane@davidson.edu>
2022-02-10 12:37:02 -0500

    Update LearningCurve.cs

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    int number = 0;
-    float long = 1.0000001;
-    string line = "hello world";
-    bool real = true;
-    /*// Start is called before the first frame update
+  public int number;
+  private string line;
+  private bool real;
+
+    // Start is called before the first frame update
     void Start()
     {
+    //Chapter 2
 
+    /* Creates 4 different variables consisting of
+     * a integer, float, string, and boolean.
+    */
+    int number = 1;
+    //float long = 5.4f;
+    string line = "Hello";
+    bool real = false;
+    Debug.Log(real);
+
+    string starter = $"{line} World";
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }*/
+    }
   }
