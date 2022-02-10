bbb6100ab486cc34aa47fafb533d89ab290deb13
Joe Kane <jokane@davidson.edu>
2022-02-10 11:52:36 -0500

    commit

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    int number = 0;
+    float long = 1.0000001;
+    string line = "hello world";
+    bool real = true;
+    /*// Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }*/
+  }
