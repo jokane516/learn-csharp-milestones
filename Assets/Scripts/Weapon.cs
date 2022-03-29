d5dd8dea6de2dc6d98f6e1795b57e45a4333eed2
Joe Kane <jokane@davidson.edu>
2022-03-29 18:02:11 -0400

    commit If Desktop is friendly?

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Weapon
+{
+    public string name;
+    public int damage;
+
+    public Weapon(string name, int damage)
+    {
+        this.name;
+        this.damage;
+    }
+}
