27971a91e17bdcc6b96b67d82d06b9a830cadce6
Joe Kane <jokane@davidson.edu>
2022-03-31 13:48:14 -0400

    Commit? Work Please?

---
+++ Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    public GameBehavior GameManager;
+    void Start()
+    {
+        GameManager = GameObject.Find("Game_Manager").
+            GetComponent<GameBehavior>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.gameObject);
+            Debug.Log("Collected");
+            GameManager.Items += 1;
+        }
+    }
+}
